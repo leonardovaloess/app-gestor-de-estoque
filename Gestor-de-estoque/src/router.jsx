4a89ffe5e476a8a2a2d0941d42ce606981333ca3
Leonardo Valões <leonardoberlandadevaloes@gmail.com>
2024-03-08 21:14:49 -0300

    finalizando projeto 2.0

--- Gestor-de-estoque/src/router.jsx
+++ Gestor-de-estoque/src/router.jsx
@@ -3,7 +3,6 @@ import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "./RootLayout";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
-import Product from "./pages/Product";
 
 const router = createBrowserRouter([
   {
